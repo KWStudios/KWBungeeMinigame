c828ee36b8dd0519155b7e61eede18a3fc9a065b
Ybrin <koray@koska.at>
2016-01-22 20:08:25 +0100

    Add stopFinishedGame method

--- src/main/java/org/kwstudios/play/kwbungeeminigame/minigame/EndGame.java
+++ src/main/java/org/kwstudios/play/kwbungeeminigame/minigame/EndGame.java
@@ -1,5 +1,40 @@
 package org.kwstudios.play.kwbungeeminigame.minigame;
 
+import org.apache.commons.lang.WordUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.kwstudios.play.kwbungeeminigame.loader.PluginLoader;
+import org.kwstudios.play.kwbungeeminigame.toolbox.ConstantHolder;
+
+import com.google.common.io.ByteArrayDataOutput;
+import com.google.common.io.ByteStreams;
+
 public class EndGame {
-	
+
+	public static void stopFinishedGame() {
+		for (Player player : Bukkit.getOnlinePlayers()) {
+			player.sendMessage(ConstantHolder.KWSTUDIOS_PREFIX + "The "
+					+ WordUtils.capitalizeFully(PluginLoader.getGamevalues().getGame_type())
+					+ " Server will be shut down in 10 seconds!");
+		}
+
+		Bukkit.getServer().getScheduler().runTaskLater(PluginLoader.getInstance(), new Runnable() {
+			@Override
+			public void run() {
+				ByteArrayDataOutput out = ByteStreams.newDataOutput();
+				out.writeUTF("Connect");
+				out.writeUTF("lobby");
+				for (Player player : Bukkit.getOnlinePlayers()) {
+					player.sendPluginMessage(PluginLoader.getInstance(), "BungeeCord", out.toByteArray());
+				}
+
+				Bukkit.getServer().getScheduler().runTaskLater(PluginLoader.getInstance(), new Runnable() {
+					public void run() {
+						Bukkit.getServer().shutdown();
+					}
+				}, 20);
+			}
+		}, 200);
+	}
+
 }
--- src/main/java/org/kwstudios/play/kwbungeeminigame/toolbox/ConstantHolder.java
+++ src/main/java/org/kwstudios/play/kwbungeeminigame/toolbox/ConstantHolder.java
@@ -7,6 +7,10 @@ public class ConstantHolder {
 	public static final String MOTD_PREFIX = ChatColor.DARK_PURPLE.toString() + ChatColor.BOLD.toString() + "KWStudios"
 			+ ChatColor.RESET + ":" + " " + ChatColor.GOLD + ChatColor.ITALIC;
 	
+	public static final String KWSTUDIOS_PREFIX = ChatColor.AQUA.toString() + ChatColor.BOLD.toString() + "["
+			+ ChatColor.YELLOW.toString() + ChatColor.BOLD.toString() + "KWStudios" + ChatColor.AQUA.toString()
+			+ ChatColor.BOLD.toString() + "] " + ChatColor.RESET;
+	
 	public static final String API_URL = "https://api.kwstudios.org/minecraft/server/:server/players/:player/storedata";
 	
 	public static final String JEDIS_SERVER = "46.101.230.181";
