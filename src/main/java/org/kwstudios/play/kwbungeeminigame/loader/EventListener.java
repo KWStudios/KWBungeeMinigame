d3a9b3dd8c8bb16e891222d589b8033ceac49881
Ybrin <koray@koska.at>
2016-01-30 14:32:54 +0100

    Code optimization and cleanup

--- src/main/java/org/kwstudios/play/kwbungeeminigame/loader/EventListener.java
+++ src/main/java/org/kwstudios/play/kwbungeeminigame/loader/EventListener.java
@@ -88,34 +88,23 @@ public final class EventListener implements Listener {
 			}
 		}, 1);
 
-		Bukkit.getServer().getScheduler().runTaskLaterAsynchronously(PluginLoader.getInstance(), new Runnable() {
-			@Override
-			public void run() {
-				Gson gson = new Gson();
-				LobbyResponse response = new LobbyResponse();
-				response.setAction(MinigameAction.UPDATE.getText());
-				response.setCurrentPlayers(players);
-				response.setGameType(PluginLoader.getGamevalues().getGame_type());
-				response.setMapName(PluginLoader.getGamevalues().getMap_name());
-				response.setServerName(PluginLoader.getGamevalues().getServer_name());
-				String message = gson.toJson(response);
-				JedisMessageSender.sendMessageToChannel(PluginLoader.getJedisValues().getHost(),
-						PluginLoader.getJedisValues().getPort(), PluginLoader.getJedisValues().getPassword(),
-						PluginLoader.getJedisValues().getChannelToSend(), message);
-			}
-		}, 1);
+		MinigameMessageHandler.sendUpdateMessage(players);
 	}
 
 	@EventHandler
 	public void onPlayerQuit(PlayerQuitEvent event) {
 		GameVariables.playersWithTPOutLobby.remove(event.getPlayer());
 		GameVariables.playersWithTPOutAndInLobby.remove(event.getPlayer());
+
+		final int players = Bukkit.getOnlinePlayers().size() - 1;
+
 		if (Bukkit.getOnlinePlayers().size() <= 1) {
 			System.out.println("the server is now empty");
 			if (GameVariables.isRunning) {
 				System.out.println("shuting down instantly");
 				Bukkit.getServer().shutdown();
 			} else {
+				MinigameMessageHandler.sendUpdateMessage(players);
 				System.out.println("starting shutdowncountdown");
 				lobbyShutdown = Bukkit.getScheduler().runTaskLaterAsynchronously(PluginLoader.getInstance(),
 						new Runnable() {
@@ -137,23 +126,7 @@ public final class EventListener implements Listener {
 						}, 2400);
 			}
 		} else {
-			final int players = Bukkit.getOnlinePlayers().size() - 1;
-			Bukkit.getServer().getScheduler().runTaskLaterAsynchronously(PluginLoader.getInstance(), new Runnable() {
-				@Override
-				public void run() {
-					Gson gson = new Gson();
-					LobbyResponse response = new LobbyResponse();
-					response.setAction(MinigameAction.UPDATE.getText());
-					response.setCurrentPlayers(players);
-					response.setGameType(PluginLoader.getGamevalues().getGame_type());
-					response.setMapName(PluginLoader.getGamevalues().getMap_name());
-					response.setServerName(PluginLoader.getGamevalues().getServer_name());
-					String message = gson.toJson(response);
-					JedisMessageSender.sendMessageToChannel(PluginLoader.getJedisValues().getHost(),
-							PluginLoader.getJedisValues().getPort(), PluginLoader.getJedisValues().getPassword(),
-							PluginLoader.getJedisValues().getChannelToSend(), message);
-				}
-			}, 1);
+			MinigameMessageHandler.sendUpdateMessage(players);
 		}
 	}
 
--- src/main/java/org/kwstudios/play/kwbungeeminigame/minigame/MinigameMessageHandler.java
+++ src/main/java/org/kwstudios/play/kwbungeeminigame/minigame/MinigameMessageHandler.java
@@ -1,6 +1,7 @@
 package org.kwstudios.play.kwbungeeminigame.minigame;
 
 import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitTask;
 import org.kwstudios.play.kwbungeeminigame.json.LobbyResponse;
 import org.kwstudios.play.kwbungeeminigame.json.MinigameAction;
 import org.kwstudios.play.kwbungeeminigame.loader.PluginLoader;
@@ -37,6 +38,25 @@ public class MinigameMessageHandler {
 		});
 		thread.start();
 		return thread;
+	}
+
+	public static BukkitTask sendUpdateMessage(final int currentPlayers) {
+		return Bukkit.getServer().getScheduler().runTaskAsynchronously(PluginLoader.getInstance(), new Runnable() {
+			@Override
+			public void run() {
+				Gson gson = new Gson();
+				LobbyResponse response = new LobbyResponse();
+				response.setAction(MinigameAction.UPDATE.getText());
+				response.setCurrentPlayers(currentPlayers);
+				response.setGameType(PluginLoader.getGamevalues().getGame_type());
+				response.setMapName(PluginLoader.getGamevalues().getMap_name());
+				response.setServerName(PluginLoader.getGamevalues().getServer_name());
+				String message = gson.toJson(response);
+				JedisMessageSender.sendMessageToChannel(PluginLoader.getJedisValues().getHost(),
+						PluginLoader.getJedisValues().getPort(), PluginLoader.getJedisValues().getPassword(),
+						PluginLoader.getJedisValues().getChannelToSend(), message);
+			}
+		});
 	}
 
 }
