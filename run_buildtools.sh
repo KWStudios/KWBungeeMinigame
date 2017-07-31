e303b8806b5c6086d09622d076eebeaf3bc2a161
Ybrin <koray@koska.at>
2017-07-31 21:23:29 +0200

    Add maven dependency caching for travis for faster builds

--- run_buildtools.sh
+++ run_buildtools.sh
@@ -13,11 +13,20 @@ git config --global --unset core.autocrlf
 
 # Run BuildTools for all needed versions
 VERSIONS[0]="1.8.8"
+MAVEN_VERSIONS[0]="R0.1-SNAPSHOT"
 
-for i in "${VERSIONS[@]}"
+for (( i=0; i<${#VERSIONS[@]}; i++ ))
 do
+  if [ -e `echo $HOME/.m2/repository/org/spigotmc/spigot/${VERSIONS[$i]}-${MAVEN_VERSIONS[$i]}` ]
+  then
+    echo "${VERSIONS[$i]} already exists in the local maven cache. Skipping this build..."
+    continue
+  else
+    echo "${VERSIONS[$i]} does not exist in the local maven cache. Building it now..."
+  fi
+
   # Run BuildTools.jar for the version $i
-  java -jar -Xms1G -Xmx3G BuildTools.jar --rev `echo $i`
+  java -jar -Xms1G -Xmx3G BuildTools.jar --rev `echo ${VERSIONS[$i]}`
 
   # Delete all files but BuildTools.jar
   TMP_JAR=`tar -c BuildTools.jar | base64`
